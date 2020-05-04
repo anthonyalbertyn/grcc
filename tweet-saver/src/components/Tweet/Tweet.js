2487b8d99b9ae993ec295fa780c4a35122731c38
Anthony Albertyn <anthonyalbertyn@gmail.com>
2020-05-04 14:05:48 +0100

    Add styling and markup for Tweet

--- tweet-saver/src/components/Tweet/Tweet.js
+++ tweet-saver/src/components/Tweet/Tweet.js
@@ -1,7 +1,35 @@
 import React from "react";
 
-const Tweet = () => {
-  return <p>Tweet</p>;
+const Tweet = ({
+  imageURL = "",
+  screenName = "",
+  name = "",
+  date = "",
+  text = "",
+}) => {
+  return (
+    <div className="tweet">
+      <div className="tweet-avatar-wrapper">
+        {imageURL && (
+          <img className="tweet-avatar" src={imageURL} alt={screenName} />
+        )}
+      </div>
+      <div className="tweet-details-wrapper">
+        <div className="tweet-name-date-wrapper">
+          <div className="tweet-name-wrapper">
+            <span className="tweet-name">{name}</span>
+            <span className="tweet-screen-name">
+              {screenName && `@${screenName}`}
+            </span>
+          </div>
+          <div className="tweet-date">{date}</div>
+        </div>
+        <div className="tweet-text">{text}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Tweet;
+
+// 1588592813000
